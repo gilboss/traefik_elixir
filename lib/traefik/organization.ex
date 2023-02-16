13a8548ad6c3ea87cbbf18d705d6f29cad6fffbd
Gilberto Reyes <gilbott@MacBook-Pro-de-Gilberto.local>
2023-02-16 16:03:32 -0600

    add short format of anonymous functions

--- lib/traefik/organization.ex
+++ lib/traefik/organization.ex
@@ -1,19 +1,33 @@
 defmodule Traefik.Organization do
   alias Traefik.Developer
-  @devs_file Path.expand("../../") |> Path.join("MOCK_DATA.csv")
+  @devs_file Path.expand("./") |> Path.join("MOCK_DATA.csv")
 
   def list_developers do
     @devs_file
     |> File.read!()
     |> String.split("\n")
     |> Kernel.tl()
-    |> Enum.map(fn e -> String.split(e) end)
-    |> Enum.map(fn e -> transform_developer(e) end)
+    |> Enum.map(&String.split(&1, ","))
+    |> Enum.map(&transform_developer/1)
+  end
+
+  def get_developer(id) when is_binary(id) do
+    id
+    |> String.to_integer()
+    |> get_developer()
+  end
+
+  def get_developer(id) when is_integer(id) and id > 0 do
+    list_developers()
+    |> Enum.find(fn
+      nil -> false
+      dev -> dev.id == id
+    end)
   end
 
   defp transform_developer([id, first_name, last_name, email, gender, ip]) do
     %Developer{
-      id: id,
+      id: String.to_integer(id),
       first_name: first_name,
       last_name: last_name,
       email: email,
